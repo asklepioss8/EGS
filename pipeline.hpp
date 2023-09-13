6c53ebb91368e2ef599ac303aca19ba8b1abb807
Asklepioss8 <kemaladana0123@gmail.com>
2023-09-13 22:00:10 +0300

    changes

--- pipeline.hpp
+++ pipeline.hpp
@@ -11,20 +11,40 @@ struct PipelineConfigInfo
 	
 	VkPipelineViewportStateCreateInfo viewportInfo{};
 	VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo{};
-	VkPipelineRasterizationStateCreateInfo rasterizationýnfo{};
+	VkPipelineRasterizationStateCreateInfo rasterizationInfo{};
 	VkPipelineMultisampleStateCreateInfo multisampleInfo{};
 	VkPipelineColorBlendAttachmentState colorBlendAttachment{};
 	VkPipelineColorBlendStateCreateInfo colorBlendInfo{};
+	VkPipelineDepthStencilStateCreateInfo depthStencilInfo{};
+	
+	VkPipelineLayout pipelineLayout = nullptr;
+	VkRenderPass renderPass = nullptr;
+	uint32_t subpass = 0;
 };
 
 
 class GraphicsPipeline
 {
 public:
-	GraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath);
+
+	GraphicsPipeline(Devices& device, const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo& configInfo);
+	~GraphicsPipeline();
+
+	// Non construction-copyable
+	GraphicsPipeline(const GraphicsPipeline&) = delete;
+	// non copyable (GraphicsPipeline& -> void maybe)
+	GraphicsPipeline& operator=(const GraphicsPipeline&) = delete;
+
+	static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t heigth);
 
 private:
-	static std::vector<char> readFile(const std::string filePath);
+	Devices& devices;
+	VkPipeline graphicsPipeline;
+	VkShaderModule vertShaderModule;
+	VkShaderModule fragShaderModule;
 
-	void createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath);
+
+	static std::vector<char> readFile(const std::string filePath);
+	void createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo& configInfo);
+	void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
 };
